24e94812b82f622f0ff12a31e33ff521a55bdbb2
Jay Deverett <jayjdeverett@gmail.com>
2017-10-23 17:08:05 -0400

    add new store form and flash messages

---
+++ starter-files/controllers/storeController.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => {
+  req.flash('error', 'Something happened');
+  req.flash('info', 'Something happened');
+  req.flash('warning', 'Something happened');
+  req.flash('success', 'Something happened');
+  res.render('index');
+}
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect('/');
+};
