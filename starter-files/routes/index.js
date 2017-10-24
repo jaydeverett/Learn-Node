db4f95cbdaf3896c62bd9d9ca65cbeb6ad038fbb
Jay Deverett <jayjdeverett@gmail.com>
2017-10-24 19:26:53 -0400

    integrate google searchable map

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -51,7 +51,11 @@ router.post('/account/reset/:token',
   authController.confirmedPasswords,
   catchErrors(authController.update)
 );
+router.get('/map', storeController.mapPage);
 
 router.get('/api/search', catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
+
+
 
 module.exports = router;
