e3cc644405cb5c4b7d808ffbe48ebd458411ad0d
Jay Deverett <jayjdeverett@gmail.com>
2017-10-24 12:18:55 -0400

    integrate google maps static api in address search

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -2,21 +2,43 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  req.flash('error', 'Something happened');
-  req.flash('info', 'Something happened');
-  req.flash('warning', 'Something happened');
-  req.flash('success', 'Something happened');
   res.render('index');
-}
+};
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
+  const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
-  res.redirect('/');
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  //Query DB for list of all stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  //1. find the store given the id
+  const store = await Store.findOne({ _id: req.params.id });
+  //2. confirm they are the owner of the store
+  //TODO
+  //3. Render out the edit form so the user can update the store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  //set the lcoation data to be a point
+  req.body.location.type = 'Point';
+  //find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, //return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+  //redirect them to the store and tell them it worked
 };
