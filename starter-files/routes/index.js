9bee9944d6c85aded657134d1887dd936a71c658
Jay Deverett <jayjdeverett@gmail.com>
2017-10-24 20:03:08 -0400

    allow user to heart stores and see list of hearted stores

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -52,9 +52,11 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)
 );
 router.get('/map', storeController.mapPage);
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
+router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 
 
 
